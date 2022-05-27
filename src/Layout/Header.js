b14c541db7144f80c6fa462df24399806e8b9d4a
sharmakavya281105 <kavyarahul9610@gmail.com>
2022-05-27 08:21:20 +0530

    d

--- src/Layout/Header.js
+++ src/Layout/Header.js
@@ -2,17 +2,20 @@
 
 
 
-import React, { Fragment, useEffect, useState } from 'react'
+import React, { Fragment, useEffect, useLayoutEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import classes from './Header.module.css'
 import socket from '../socket/socket'
+import {FaReact} from 'react-icons/fa'
 
 
-const tapped = false
+
+var tapped = false
 
 export default function Header() {
-    const [isShown,setIsShown] = useState(false)
+    const [linkVis,setLinkVis] = useState(true)
     const [top, setTop] = useState(0)
+    const [hold,setHold]=useState(false)
     // useEffect(()=>{
     //     // window.onscroll = (e)=>{
     //     //     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
@@ -27,24 +30,60 @@ export default function Header() {
     //         setIsShown(true)
     //     })
     // } ,[])
+    const [loc,setLoc] = useState({})
 
-    const onClickHandler = ()=>{
-        if(!tapped){
-          tapped=true
-          return
-        } else {
-
-        }
-
+    useLayoutEffect(()=>{
+      var options = {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0
+      };
+      
+      function success(pos) {
+        var crd = pos.coords;
+      
+        console.log('Your current position is:');
+        console.log(`Latitude : ${crd.latitude}`);
+        console.log(`Longitude: ${crd.longitude}`);
+        console.log(`More or less ${crd.accuracy} meters.`);
+        setLoc({
+          lat:crd.latitude,
+          lng:crd.longitude
+        })
+      }
+      
+      function error(err) {
+        console.warn(`ERROR(${err.code}): ${err.message}`);
+        setLoc(err)
+      }
+      
+      navigator.geolocation.getCurrentPosition(success, error, options);
+      
+    },[])
+    function toggleLinks(){
+      setLinkVis(s=>{return !s})
+      console.log(linkVis);
+      
     }
+
+
   return (
     <Fragment>
-        <header className={classes['header']} style={{top:top}}>
-          
-            <Link className={classes['links']} to="/about">About</Link>
+        <header  className={classes['header']} >
+          <div className={classes['logo-container']} >
+              <FaReact className={classes['logo']}  size={40} />
+          </div>
+          <div className={classes['menu-btn']} onClick={()=>{toggleLinks()}}>
+               <span></span>
+               <span></span>
+               <span></span>
+          </div>
+          <div className={classes['links-container']} style={linkVis?{display:'flex'}:{}}>
+            <Link className={classes['links']} to="/about">{JSON.stringify(loc)}</Link>
             <Link className={classes['links']} to="/">Home</Link>
-            {<Link className={classes['links']} to="/hidden">Hidden</Link>}
-
+            <Link className={classes['links']} to="/register">Register</Link>
+            <Link className={classes['links']} to="/hidden">Hidden</Link>
+          </div>
         </header>
     </Fragment>
   )
